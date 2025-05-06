729ab2e575bdd14671c91caae0cd77d7a9f2ccfe
Kateko Tivane <119723087+kateko60@users.noreply.github.com>
2025-05-06 16:09:12 +0200

    Add files via upload

---
+++ appointment_booking.java
@@ -0,0 +1,24 @@
+package com.example.healthapp;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
+
+public class appointment_booking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_appointment_booking);
+
+    }
+}
